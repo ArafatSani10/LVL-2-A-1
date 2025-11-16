5ad747d73adcb4de335426e0c209749e54d517be
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-11-17 02:48:32 +0600

    fixed all problem.

--- solution.ts
+++ solution.ts
@@ -52,11 +52,8 @@ class Person {
     }
 }
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
 
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
+
 
 
 
